52d8c8d29337fe12b09bc1210c17699467a66d6d
JhojanFlp <pipeflx85@gmail.com>
2019-04-12 15:58:46 -0500

    Config

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,3 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+process.env.URLDB = 'mongodb://localhost:27017/cursoNodeJs';
 
-let urlDB
-if (process.env.NODE_ENV === 'local'){
-	urlDB = 'mongodb://localhost:27017/cursoNodeJs';
-}
-else {
-	urlDB = 'mongodb+srv://Pipe:mimaxs85@cursonodejs-ojkjh.mongodb.net/cursoNodeJs?retryWrites=true'
-}
-
-process.env.URLDB = urlDB
